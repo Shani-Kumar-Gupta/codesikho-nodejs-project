ab70223e847d3f943dcbc17b023b93a137b086db
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-04-11 20:05:04 +0530

    basics of middleware done

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,35 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
 
+// Basic of Middleware
+
+// Common Middleware
+app.use((req,res,next) => {
+    console.log("common middleware called");
+    next();
+})
+// Middleware used for login route
+app.use('/login',(req,res,next) => {
+    console.log("middleware called");
+    next();
+})
+
 // Basic of Routing
 
 app.get('/login', (req,res)=> {
     res.send("successfully logged in");
+})
+
+app.get('/middleware', (req: any,res,next) => {
+    const data = {
+        fullName: "Shani Kumar Gupta",
+        college: "GLA University", 
+    }
+    req.user = data;
+    next();
+}, (req: any, res, next) => {
+    console.log('called another middleware');
+    res.send(req.user);
 })
 
 app.get('/api/test', (req,res)=> {
