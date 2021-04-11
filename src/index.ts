763a1aa89ed5c073dad445cf6b4f5693c1970d11
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-04-11 17:23:40 +0530

    basic of routing

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,13 @@ const port = 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
+
+// Basic of Routing
+
+app.get('/login', (req,res)=> {
+    res.send("successfully logged in");
+})
+
+app.get('/api/test', (req,res)=> {
+    res.send("this is a test request");
+})
